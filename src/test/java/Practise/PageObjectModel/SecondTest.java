f5f63c033e072214984e90ffc1976f3203be0e2f
svahab <vahabvahu2429@gmail.com>
2026-04-08 22:26:10 +0530

    Commit Second test to BranchX

---
+++ src/test/java/Practise/PageObjectModel/SecondTest.java
@@ -0,0 +1,12 @@
+package Practise.PageObjectModel;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	
+	@Test
+	public void secondMethodTest() {
+		System.out.println("Second test");
+	}
+
+}
