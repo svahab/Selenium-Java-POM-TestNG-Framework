1e88ec03fbbbc045db9c79837bed75a0746d4cce
svahab <vahabvahu2429@gmail.com>
2026-04-08 20:26:14 +0530

    Second Test added

---
+++ src/test/java/Practise/PageObjectModel/SecondTest.java
@@ -0,0 +1,12 @@
+package Practise.PageObjectModel;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	
+	@Test
+	public void TestSoftwre() {
+		System.out.println("Test softwre");
+	}
+
+}
